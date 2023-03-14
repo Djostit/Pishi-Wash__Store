c878a25e3b7a411042048daa8310636387c8ae4b
Djostit <djostit@mail.ru>
2023-03-14 23:18:39 +0500

    Upd #18

--- Pishi-Wash__Store/Data/TradeContext.cs
+++ Pishi-Wash__Store/Data/TradeContext.cs
@@ -30,11 +30,6 @@ public partial class TradeContext : DbContext
     public virtual DbSet<Role> Roles { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;user=pishi_;password=JehypNk6nm52oq;database=trade", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
